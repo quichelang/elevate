1e8560056604d5e89ab04f6a46e65cf4c0c7897b
Jag Chadha <jagtesh@gmail.com>
2026-02-05 22:54:19 -0500

    chore: ignore and untrack lexopt example build artifacts

--- examples/lexopt-elevate/target/debug/deps/lexopt_elevate-36afdae292a1d20f.d
+++
@@ -1,8 +0,0 @@
-/Volumes/Dev/code/jagtesh/elevate/examples/lexopt-elevate/target/debug/deps/lexopt_elevate-36afdae292a1d20f.d: src/lib.rs src/model.rs src/parser.rs src/runtime.rs
-
-/Volumes/Dev/code/jagtesh/elevate/examples/lexopt-elevate/target/debug/deps/lexopt_elevate-36afdae292a1d20f: src/lib.rs src/model.rs src/parser.rs src/runtime.rs
-
-src/lib.rs:
-src/model.rs:
-src/parser.rs:
-src/runtime.rs:
